7d64f33df3fd76841f8b45d44cd4252bbe8ec0c9
mrlanu <mrlanu@gmail.com>
2023-11-23 13:30:58 -0600

    fix(client): construction widget

--- client/lib/buildings/view/building_widgets/construction.dart
+++ client/lib/buildings/view/building_widgets/construction.dart
@@ -16,51 +16,53 @@ class Construction extends StatelessWidget {
       builder: (context, state) {
         final upgradingTask = state.settlement!.constructionTasks
             .where((task) => task.position == buildingRecord[0])
-            .first;
-        final building = buildingSpecefication[upgradingTask.buildingId]!;
-        return Card(
-            elevation: 5,
-            child: Container(
-              padding: const EdgeInsets.all(15),
-              height: 390,
-              child: Column(
-                children: [
-                  Align(
-                      alignment: Alignment.topLeft,
-                      child: Text(
-                        '${building.name} to level ${upgradingTask.toLevel}',
-                        style: Theme.of(context).textTheme.titleLarge,
-                      )),
-                  const Divider(),
-                  Expanded(
-                      child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
+            .toList();
+        return upgradingTask.isEmpty
+            ? const SizedBox()
+            : Card(
+                elevation: 5,
+                child: Container(
+                  padding: const EdgeInsets.all(15),
+                  height: 390,
+                  child: Column(
                     children: [
-                      Text(
-                        'Ready in:',
-                        style: Theme.of(context).textTheme.titleLarge,
-                      ),
-                      CountdownTimer(
-                        startValue: upgradingTask.executionTime
-                            .difference(DateTime.now())
-                            .inSeconds,
-                        onFinish: () {
-                          context
-                              .read<BuildingsBloc>()
-                              .add(const SettlementFetchRequested());
-                        },
-                        textStyle: Theme.of(context).textTheme.titleLarge,
-                      ),
+                      Align(
+                          alignment: Alignment.topLeft,
+                          child: Text(
+                            '${buildingSpecefication[upgradingTask[0].buildingId]!.name} to level ${upgradingTask[0].toLevel}',
+                            style: Theme.of(context).textTheme.titleLarge,
+                          )),
+                      const Divider(),
+                      Expanded(
+                          child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Ready in:',
+                            style: Theme.of(context).textTheme.titleLarge,
+                          ),
+                          CountdownTimer(
+                            startValue: upgradingTask[0]
+                                .executionTime
+                                .difference(DateTime.now())
+                                .inSeconds,
+                            onFinish: () {
+                              context
+                                  .read<BuildingsBloc>()
+                                  .add(const SettlementFetchRequested());
+                            },
+                            textStyle: Theme.of(context).textTheme.titleLarge,
+                          ),
+                        ],
+                      )),
+                      const Divider(),
+                      IconButton.outlined(
+                          color: Colors.red,
+                          onPressed: () {},
+                          icon: const Icon(Icons.cancel_outlined))
                     ],
-                  )),
-                  const Divider(),
-                  IconButton.outlined(
-                      color: Colors.red,
-                      onPressed: () {},
-                      icon: const Icon(Icons.cancel_outlined))
-                ],
-              ),
-            ));
+                  ),
+                ));
       },
     );
   }
