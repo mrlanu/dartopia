425b0d90223da15a6a8b9b1ee3707fd25681972f
mrlanu <mrlanu@gmail.com>
2024-02-25 11:56:46 -0600

    refactor: grid item

--- client/lib/buildings/view/buildings_page_grid.dart
+++ client/lib/buildings/view/buildings_page_grid.dart
@@ -72,19 +72,15 @@ class _BuildingsGridViewState extends State<BuildingsGridView> {
                 .where((bR) =>
                     bR[1] == 0 || bR[1] == 1 || bR[1] == 2 || bR[1] == 3)
                 .map(
-              (field) {
-                final upgradingTasks = widget.settlement.constructionTasks
-                    .where((task) => task.buildingId == field[1])
+              (fieldRecord) {
+                final constructionTasks = widget.settlement.constructionTasks
+                    .where((task) => task.buildingId == fieldRecord[1])
                     .toList();
-                final (label, labelBackground, labelColor) =
-                    _buildLabel(field, upgradingTasks.lastOrNull);
                 return _BuildingGridItem(
-                  key: ValueKey('${field[0]} ${field[1]}'),
-                  buildingRecord: field,
+                  key: ValueKey('${fieldRecord[0]} ${fieldRecord[1]}'),
+                  buildingRecord: fieldRecord,
+                  constructionTask: constructionTasks.lastOrNull,
                   prodPerHour: widget.settlement.calculateProducePerHour(),
-                  label: label,
-                  labelBackground: labelBackground,
-                  labelColor: labelColor,
                 );
               },
             ).toList(),
@@ -106,14 +102,10 @@ class _BuildingsGridViewState extends State<BuildingsGridView> {
                 final upgradingTasks = widget.settlement.constructionTasks
                     .where((task) => task.position == bRecord[0])
                     .toList();
-                final (label, labelBackground, labelColor) =
-                _buildLabel(bRecord, upgradingTasks.lastOrNull);
                 return _BuildingGridItem(
                   key: ValueKey('${bRecord[0]} ${bRecord[1]}'),
                   buildingRecord: bRecord,
-                  label: label,
-                  labelBackground: labelBackground,
-                  labelColor: labelColor,
+                  constructionTask: upgradingTasks.lastOrNull,
                 );
               },
             ).toList(),
@@ -122,63 +114,26 @@ class _BuildingsGridViewState extends State<BuildingsGridView> {
       ],
     );
   }
+}
 
-  (Widget, Color, Color) _buildLabel(
-      List<int> buildingRecord, ConstructionTask? constructionTask) {
+class _BuildingGridItem extends StatelessWidget {
+  const _BuildingGridItem({
+    super.key,
+    required this.buildingRecord,
+    this.constructionTask,
+    this.prodPerHour = const [0, 0, 0, 0],
+  });
+
+  final List<int> buildingRecord;
+  final ConstructionTask? constructionTask;
+  final List<int> prodPerHour;
+
+  @override
+  Widget build(BuildContext context) {
     final labelBackground = constructionTask == null
         ? primary
         : const Color.fromRGBO(253, 216, 87, 1.0);
     final labelColor = constructionTask == null ? Colors.white : Colors.black;
-    return constructionTask == null
-        ? (
-            Text(buildingSpecefication[buildingRecord[1]]!.name),
-            labelBackground,
-            labelColor
-          )
-        : (
-            Row(
-              children: [
-                Text(buildingSpecefication[constructionTask.buildingId]!.name),
-                const SizedBox(width: 5),
-                CountdownTimer(
-                  startValue: constructionTask.executionTime
-                      .difference(DateTime.now())
-                      .inSeconds,
-                  onFinish: () {
-                    context
-                        .read<SettlementBloc>()
-                        .add(const SettlementFetchRequested());
-                  },
-                  textStyle: Theme.of(context)
-                      .textTheme
-                      .bodyMedium!
-                      .copyWith(color: labelColor),
-                ),
-              ],
-            ),
-            labelBackground,
-            labelColor
-          );
-  }
-}
-
-class _BuildingGridItem extends StatelessWidget {
-  const _BuildingGridItem(
-      {super.key,
-      required this.buildingRecord,
-      this.prodPerHour = const [0, 0, 0, 0],
-      required this.label,
-      this.labelBackground = primary,
-      this.labelColor = Colors.black});
-
-  final List<int> buildingRecord;
-  final List<int> prodPerHour;
-  final Widget label;
-  final Color labelBackground;
-  final Color labelColor;
-
-  @override
-  Widget build(BuildContext context) {
     final id = buildingRecord[1];
     final lbl = id == 0 || id == 1 || id == 2 || id == 3
         ? prodPerHour[id].toString()
@@ -193,7 +148,7 @@ class _BuildingGridItem extends StatelessWidget {
       smallSize: 0,
       child: Badge(
         alignment: Alignment.bottomLeft,
-        label: label,
+        label: _buildLabel(labelColor, context),
         textColor: labelColor,
         backgroundColor: labelBackground,
         offset: const Offset(0, 0),
@@ -215,6 +170,31 @@ class _BuildingGridItem extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  Widget _buildLabel(Color labelColor, BuildContext context) {
+    return constructionTask == null
+        ? Text(buildingSpecefication[buildingRecord[1]]!.name)
+        : Row(
+            children: [
+              Text(buildingSpecefication[constructionTask!.buildingId]!.name),
+              const SizedBox(width: 5),
+              CountdownTimer(
+                startValue: constructionTask!.executionTime
+                    .difference(DateTime.now())
+                    .inSeconds,
+                onFinish: () {
+                  context
+                      .read<SettlementBloc>()
+                      .add(const SettlementFetchRequested());
+                },
+                textStyle: Theme.of(context)
+                    .textTheme
+                    .bodyMedium!
+                    .copyWith(color: labelColor),
+              ),
+            ],
+          );
   }
 }
 
