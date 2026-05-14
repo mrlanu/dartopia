4b515b65365d5c01fc597fcb4497575b849c7fc0
mrlanu <mrlanu@gmail.com>
2026-05-14 12:22:02 -0500

    refactor: delete buildings_page_carousel

--- client/lib/buildings/view/view.dart
+++ client/lib/buildings/view/view.dart
@@ -1,4 +1,3 @@
 export 'building_widgets/building_widgets.dart';
 export 'widgets/widgets.dart';
 export 'buildings_page_grid.dart';
-export 'buildings_page_carousel.dart';
--- client/lib/consts/consts.dart
+++ client/lib/consts/consts.dart
@@ -1,8 +1,4 @@
-const offsetForFieldsCarousel = 14;
-
 const int periodicUpdateTime = 15;
-//later, these options should be fetched from the server
-const worldWidth = 50;
 const mapWidth = 7; // should be odd number
 const maxBuildings = 30;
 
